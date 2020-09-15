17a3c596b868b29aaa4e70d57146f40c243d30be
mikevhe18 <viriyananda.michael@gmail.com>
2020-09-15 11:02:24 +0700

    [currency_exchange_method] 8.0.1.0.0

---
+++ currency_exchange_method/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Eficent
+# Copyright 2015 Techrifiv Solutions Pte Ltd
+# Copyright 2015 Statecraft Systems Pte Ltd
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import (
+    res_currency,
+)
